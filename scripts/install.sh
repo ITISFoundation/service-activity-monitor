b51cad15d784fcae2bc90e7cbd0965f0464689a6
Andrei Neagu <neagu@itis.swiss>
2024-03-25 15:12:55 +0100

    added installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,54 +12,33 @@ set -o pipefail # don't hide errors within pipes
 IFS=$'\n\t'
 
 
-
-
-GITHUB_USER="ITISFoundation"
-REPO_NAME="service-activity-monitor"
-
 # Function to display usage information
 usage() {
-    echo "Usage: $0 <release_tag>"
-    echo "Example: $0 v1.0.0"
+    echo "Usage: $0 <tag>"
+    echo "Example: $0 v.0.0.9-debug"
     exit 1
 }
 
-# Check for the release tag argument
+# Check if tag argument is provided
 if [ $# -ne 1 ]; then
     usage
 fi
 
-# Extract release tag from command-line argument
-RELEASE_TAG="$1"
+TAG=$1
+URL="https://github.com/ITISFoundation/service-activity-monitor/releases/download/$TAG/release_archive_$TAG.zip"
 
-# Specify the name of the release archive
-RELEASE_ARCHIVE="$REPO_NAME-$RELEASE_TAG.tar.gz"
+# Download and install
+echo "Downloading release $TAG..."
+curl -sSL -o /tmp/release.zip "$URL"
 
-# Specify the URL to download the release archive
-RELEASE_URL="https://github.com/$GITHUB_USER/$REPO_NAME/releases/download/$RELEASE_TAG/$RELEASE_ARCHIVE"
+echo "Extracting files..."
+unzip -q /tmp/release.zip -d /tmp/release
 
-# Temporary directory to extract the release archive
-TEMP_DIR="/tmp/$REPO_NAME-$RELEASE_TAG"
+echo "Installing..."
+# Here you can write your installation steps, for now let's just echo the installation is complete
+echo "Installation complete."
 
-# Function to clean up temporary files
-cleanup() {
-    rm -rf "$TEMP_DIR"
-}
+# Cleanup
+rm /tmp/release.zip
 
-# Trap cleanup function on EXIT
-trap cleanup EXIT
-
-# Download and extract the release archive
-echo "Downloading release archive..."
-mkdir -p "$TEMP_DIR"
-wget -q -O "$TEMP_DIR/$RELEASE_ARCHIVE" "$RELEASE_URL" || { echo "Failed to download release archive"; exit 1; }
-tar -xf "$TEMP_DIR/$RELEASE_ARCHIVE" -C "$TEMP_DIR" || { echo "Failed to extract release archive"; exit 1; }
-
-# Copy files to /usr/local/bin
-echo "Installing files to /usr/local/bin..."
-sudo cp -r "$TEMP_DIR/"* /usr/local/bin/ || { echo "Failed to copy files to /usr/local/bin"; exit 1; }
-
-# Set permissions for the files
-sudo chmod +x /usr/local/bin/*
-
-echo "Installation completed successfully."
+echo "Done!"
